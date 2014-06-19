1e16f7b4aada62c638ec332e63a56ce69fa1551c
Paopao sa <paopaosa@qq.com>
2014-06-19 15:01:54 +0800

    Initial Commit

---
+++ ipadDemo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ipadDemo
+//
+//  Created by macpps on 14-6-19.
+//  Copyright (c) 2014年 jz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "JZPAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([JZPAppDelegate class]));
+    }
+}
